007060492edee82fe1039398828ca66e0b0aaea2
PhanTrung123 <phanquangtrung30042004@gmail.com>
2025-01-23 17:48:07 +0700

    làm phần mô tả sản phẩm #2 và reposive...

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -276,3 +276,25 @@ document.addEventListener("DOMContentLoaded", () => {
   // Initialize slider
   updateSlider();
 });
+
+// chức năng kích vào các product-tab__item
+window.addEventListener("template-loaded", () => {
+  const tabsSelector = "product-tab__item";
+  const contentsSelector = "product-tab__content";
+  const tabActive = `${tabsSelector}--active`;
+  const contentActive = `${contentsSelector}--active`;
+
+  const tabContainers = $$(".js-tabs");
+  tabContainers.forEach((tabContainer) => {
+    const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+    const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+    tabs.forEach((tab, index) => {
+      tab.onclick = () => {
+        tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+        tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+        tab.classList.add(tabActive);
+        contents[index].classList.add(contentActive);
+      };
+    });
+  });
+});
